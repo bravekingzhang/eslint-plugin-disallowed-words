622b8b1ec3f5129529299ad45ae4f638f0771f54
brzhang <hoollyzhang@tencent.com>
2024-03-12 18:59:29 +0800

    feat: 包名修改

--- tests/no-disallowed-words.js
+++ tests/no-disallowed-words.js
@@ -10,14 +10,14 @@ ruleTester.run("no-disallowed-words", rule, {
     // 在这里添加符合规则的代码示例
     {
       code: "'电子签，模板，'",
-      options: [["模版", "其他不允许的词"]],
+      options: [["模版", "登陆"]],
     },
   ],
   invalid: [
     // 在这里添加违反规则的代码示例，并指定期望的错误消息
     {
       code: "'模版编辑，实际应该是模板，模板，模板'",
-      options: [["模版"]],
+      options: [["模版", "登陆"]],
       errors: [{ message: 'The word "模版" is disallowed.' }],
     },
   ],
